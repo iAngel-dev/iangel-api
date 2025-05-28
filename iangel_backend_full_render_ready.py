e09d5ad8b55982de722d7a5a2bd07854e6276968
iAngel-dev <f.lefebvre1995@icloud.com>
2025-05-28 15:53:55 -0400

    Add files via upload

---
+++ iangel_backend_full_render_ready.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bienvenue sur l'API iAngel 🕊️", 200
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
